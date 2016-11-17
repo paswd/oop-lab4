f0b7fe2dc47f8a2b7be9921f62dc9b5d4982e09b
paswd <paswd@yandex.ru>
2016-11-17 18:35:31 +0300

    Release

---
+++ TStack.h
@@ -0,0 +1,20 @@
+#ifndef TSTACK_H
+#define TSTACK_H
+#include "TStackItem.h"
+#include <iostream>
+#include <memory>
+
+template <class T> class TStack {
+public:
+	TStack();
+
+	void push(std::shared_ptr<T> &&item);
+	bool empty();
+	std::shared_ptr<T> pop();
+	template <class A> friend std::ostream& operator<<(std::ostream& os,const TStack<A>& stack);
+	virtual ~TStack();
+private:
+
+	std::shared_ptr<TStackItem<T>> head;
+};
+#endif /* TSTACK_H */
